60933b46852a196096d7a85a0787bf43876a78db
Moroz Roman <morozdef24@gmail.com>
2025-01-27 20:53:44 +0300

    db

--- app/config/config.py
+++ app/config/config.py
@@ -1,6 +1,7 @@
 import os
 import logging
 from aiogram import Bot
+from aiogram.fsm.storage.memory import MemoryStorage
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -10,4 +11,5 @@ logging.basicConfig(level=logging.INFO)
 class Config:
     logger = logging.getLogger(__name__)
     bot = Bot(token=os.getenv('BOT_TOKEN'))
-
+    DATABASE_URL = os.getenv('DATABASE_URL')
+    storage = MemoryStorage()
--- app/handlers/handlers.py
+++ app/handlers/handlers.py
@@ -1,33 +1,34 @@
+from aiogram import F
 from aiogram import Router, types
 from aiogram.filters import CommandStart, Command
+
+from keyboards.keyboards import main_kb
 from config.config import Config
 
 first_router = Router()
 
 async def set_bot_commands():
     await Config.bot.set_my_commands([
-        types.BotCommand(command="/add", description="Добавить задачу"),
-        types.BotCommand(command="/list", description="Список задач"),
-        types.BotCommand(command="/delete", description="Удалить задачу"),
-        types.BotCommand(command="/help", description="Помощь"),
+        types.BotCommand(command="/contribute", description="Contributing into project-github repository"),
+        types.BotCommand(command="/review", description="Send a review to author"),
+        types.BotCommand(command="/donate", description="Donate"),
     ])
 
 @first_router.message(CommandStart())
 async def start_command(message: types.Message):
-    await message.reply("📝 Добро пожаловать в To-Do List бот!\n"
-                        "Используйте команды:\n"
-                        "/add - добавить задачу\n"
-                        "/list - показать список задач\n"
-                        "/delete - удалить задачу\n"
-                        "/help - справка")
+    await message.answer(
+        "📝 Welcome to the To-Do List bot!\n"
+        "Use the menu on the left or the button below:",
+        reply_markup=main_kb()
+    )
     await set_bot_commands()
     Config.logger.info(f"Команда /start от пользователя {message.from_user.id}")
     
 @first_router.message(Command('help'))
 async def help_command(message: types.Message):
-    await message.reply("ℹ️ Доступные команды:\n"
-                        "/add - добавить новую задачу\n"
-                        "/list - показать все задачи\n"
-                        "/delete - удалить задачу по номеру\n"
-                        "/help - показать это сообщение")
+    await message.answer("ℹ️ Available commands:\n:\n"
+                        "/add - add a new task\n"
+                        "/list - show all tasks\n"
+                        "/delete - delete task by number\n"
+                        "/help - show this message")
     Config.logger.info(f"Команда /help от пользователя {message.from_user.id}")
